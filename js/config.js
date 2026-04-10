79f5c0facf46b3d92fce2e69770ac61952cc77db
Mark-Ferdi <a.markferdi@gmail.com>
2026-04-10 19:52:14 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 const SUPABASE_URL = 'https://qawxxgofajychkujnkmw.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhd3h4Z29mYWp5Y2hrdWpua213Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3OTc2NTgsImV4cCI6MjA5MTM3MzY1OH0.smdzsgu6s3xH1GCYCbFlahY-688h6iWd-ReKc8uK_0M';
 
-const supabase = window.supabase.createClient(https://qawxxgofajychkujnkmw.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhd3h4Z29mYWp5Y2hrdWpua213Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3OTc2NTgsImV4cCI6MjA5MTM3MzY1OH0.smdzsgu6s3xH1GCYCbFlahY-688h6iWd-ReKc8uK_0M);
-
+const { createClient } = window.supabase;
+window.supabase = createClient(https://qawxxgofajychkujnkmw.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhd3h4Z29mYWp5Y2hrdWpua213Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3OTc2NTgsImV4cCI6MjA5MTM3MzY1OH0.smdzsgu6s3xH1GCYCbFlahY-688h6iWd-ReKc8uK_0M);
 // Storage bucket name (create in Supabase Storage)
 const STORAGE_BUCKET = 'item-images';
